955c6d231e50fd41493c29dd6d66d8de7a2f1a9c
Morgan Visnesky <morganticss@gmail.com>
2022-04-10 14:24:35 -0400

    adding test bench for each component

---
+++ pitt-traditions-arduino-ws/component_test_benches/phototransistor_test/phototransitor_test.ino
@@ -0,0 +1,15 @@
+/**************************************************************
+ * AUTHOR(S):    Morgan Visnesky
+ * DATE:         04/10/2022
+ * FILENAME:     phototransistor_test.ino
+ * ORGANIZATION: Junior Design Team CHI
+ * TEAM MEMBERS: 
+ *              Chris Guiher 
+ *              Alex Shuster
+ *              Morgan Visnesky
+ *              David Cramer
+ *
+ * DESCRIPTION: 
+ *     Simple test code to ensure phototransitor functions properly.
+ * NOTES:
+ **************************************************************/
