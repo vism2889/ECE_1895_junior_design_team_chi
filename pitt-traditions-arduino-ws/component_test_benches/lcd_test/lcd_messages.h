9c4f625010195a7a299798ba1a021a1a044fe1ff
Morgan Visnesky <morganticss@gmail.com>
2022-04-12 13:46:02 -0400

    adding correct message  functionality

---
+++ pitt-traditions-arduino-ws/component_test_benches/lcd_test/lcd_messages.h
@@ -0,0 +1,74 @@
+/**************************************************************
+ * AUTHOR(S):    Morgan Visnesky
+ * DATE:         04/10/2022
+ * FILENAME:     lcd_messages.h
+ * ORGANIZATION: Junior Design Team CHI
+ * TEAM MEMBERS: 
+ *              Chris Guiher 
+ *              Alex Shuster
+ *              Morgan Visnesky
+ *              David Cramer
+ *
+ * DESCRIPTION: 
+ *     Stores the different messages to be displayed with the LCD
+ * NOTES:
+ **************************************************************/
+
+#include <Wire.h>
+#include "DFRobot_LCD.h"
+
+DFRobot_LCD lcd(16,2);  //16 characters and 2 lines of show
+
+const int score = 25;
+
+ void hailToPittMessage(){
+    lcd.init();
+    lcd.setCursor(5,0);
+    lcd.print("CHEER");
+    lcd.setCursor(0,1);
+    lcd.print("HAIL TO PITT");
+ }
+
+ void lightVictoryLightMessage(){
+    lcd.init();
+    lcd.setCursor(3,0);
+    lcd.print("Light the");
+    lcd.setCursor(1,1);
+    lcd.print("Victory Light");
+ }
+
+ void panthersNoseMessage(){
+    lcd.init();
+    lcd.setCursor(4,0);
+    lcd.print("Rub the");
+    lcd.setCursor(1,1);
+    lcd.print("Panthers Nose");
+ }
+
+void welcomeMessage(){
+    lcd.init();
+    lcd.setCursor(0,0);
+    lcd.print("Pitt Traditions");
+    lcd.setCursor(2,1);
+    lcd.print("Bop-It Game");
+}
+
+void roundSuccessMessage(){
+    lcd.init();
+    lcd.setCursor(5,0);
+    lcd.print("HORRAY");
+    lcd.setCursor(2,1);
+    int score = 25;
+    String score_ = String(score);
+    lcd.print("Score: " + score_);
+}
+
+void roundFailMessage(){
+    lcd.init();
+    lcd.setCursor(0,0);
+    lcd.print("OOPS! Try Again!");
+    lcd.setCursor(2,1);
+    int score = 25;
+    String score_ = String(score);
+    lcd.print("Score: " + score_);
+}
--- pitt-traditions-arduino-ws/component_test_benches/lcd_test/lcd_test.ino
+++ pitt-traditions-arduino-ws/component_test_benches/lcd_test/lcd_test.ino
@@ -1,18 +1,37 @@
+/**************************************************************
+ * AUTHOR(S):    Morgan Visnesky
+ * DATE:         04/10/2022
+ * FILENAME:     lcd_test.ino
+ * ORGANIZATION: Junior Design Team CHI
+ * TEAM MEMBERS: 
+ *              Chris Guiher 
+ *              Alex Shuster
+ *              Morgan Visnesky
+ *              David Cramer
+ *
+ * DESCRIPTION: 
+ *     Simple test code to ensure lcd display functions properly
+ * NOTES:
+ **************************************************************/
+
+
 #include <Wire.h>
 #include "DFRobot_LCD.h"
+#include "lcd_messages.h"
 
 char dtaUart[15];
 char dtaLen = 0;
 
-DFRobot_LCD lcd(16,2);  //16 characters and 2 lines of show
+//DFRobot_LCD lcd(16,2);  //16 characters and 2 lines of show
 
 void setup() {
   Serial.begin(115200);
     lcd.init();
-    lcd.setCursor(0,0);
-    lcd.print("Pitt Traditions");
-    lcd.setCursor(0,1);
-    lcd.print("Score: ");
+    //lightVictoryLightMessage();
+    // lcd.setCursor(0,0);
+    // lcd.print("Pitt Traditions");
+    // lcd.setCursor(0,1);
+    // lcd.print("Score: ");
 }
 
 void loop() {
@@ -28,9 +47,21 @@ void loop() {
         lcd.noBlinkLED();
     }
     
-    String num = String(millis()/1000);
-    lcd.setCursor(0,1);
-    lcd.print("Score: " + num);
+    // String num = String(millis()/1000);
+    // lcd.setCursor(0,1);
+    // lcd.print("Score: " + num);
+    hailToPittMessage();
+    delay(1000);
+    lightVictoryLightMessage();
+    delay(1000);
+    panthersNoseMessage();
+    delay(1000);
+    welcomeMessage();
+    delay(1000);
+    roundSuccessMessage();
+    delay(1000);
+    roundFailMessage();
+    delay(1000);
 }
 
 void serialEvent() {
