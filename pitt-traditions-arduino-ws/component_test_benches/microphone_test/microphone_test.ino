ccd690157c9e2e4982d5b861ddae0dc5a662e59d
Morgan Visnesky <morganticss@gmail.com>
2022-04-11 22:15:01 -0400

    adding mic test functionality

--- pitt-traditions-arduino-ws/component_test_benches/microphone_test/microphone_test.ino
+++ pitt-traditions-arduino-ws/component_test_benches/microphone_test/microphone_test.ino
@@ -13,3 +13,51 @@
  *     Simple test code to ensure microphone functions properly.
  * NOTES:
  **************************************************************/
+
+ boolean readFromMicrophone(int pAnalogPin)
+{
+  unsigned long startMillis = millis();  // Start of sample window
+  const int sampleWindow    = 50; // Sample window width in mS (50 mS = 20Hz)
+  int micVal                = 0;
+  boolean cheer             = false;
+  unsigned long startMillis = millis();  // Start of sample window
+  unsigned int peakToPeak   = 0;   // peak-to-peak level
+  unsigned int signalMax    = 0;
+  unsigned int signalMin    = 1024;
+
+  while (millis() - startMillis < sampleWindow){
+    sample = analogRead(pAnalogPin);
+    if (sample < 1024){  // toss out spurious readings
+       if (sample > signalMax){
+         signalMax = sample;  // save just the max levels
+       }
+       else if (sample < signalMin){
+         signalMin = sample;  // save just the min levels
+       }
+     }
+   }
+   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+   double micVal = (peakToPeak * 5.0) / 1024;  // convert to volts
+
+   /* 
+    *  For ambient noise, peakToPeak voltage tends to be near negligible. 
+    *  When a user speaks near the microphone, peakToPeak increases significantly. 
+    *  Can set a minimum threshold (2V?) to successfully show a user input
+    */
+    
+  if (micVal > 2){cheer = true;}
+  else{cheer = false;}
+
+  return cheer;
+}
+
+const int micPin = 3;
+
+void setup() {
+    pinMode(micPin, INPUT);
+}
+
+void loop() {
+    delay(1000);
+    readFromMicrophone(micPin);
+}
